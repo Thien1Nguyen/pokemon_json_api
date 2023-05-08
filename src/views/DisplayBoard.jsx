593945e9c73b832c67926d9bcf1d33d01248ec55
Thien1Nguyen <tnguyen5219@gmail.com>
2023-05-08 12:33:28 -0700

    First Commit

---
+++ src/views/DisplayBoard.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+
+const PokeDex = () => {
+    const [pokemonList, setPokemonList] = useState([]);
+
+
+    const FetchPokemon = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+        .then(response =>{
+            return response.json()
+        })
+        .then(response => {
+            setPokemonList(response.results)
+        })
+        .catch(err => {
+            console.log("Error: ", err);
+        })
+    }
+
+
+
+
+
+    return (
+        <div className='container text-center'>
+            <button onClick={FetchPokemon} className='mb-5'> Fetch Pokemon</button>
+            {
+                pokemonList.map( (pokemon, i) => {
+                    return(
+                        <li key = {i}>
+                            {pokemon.name}
+                        </li>
+                    )
+                })
+            }
+        </div>
+    )
+        }
+
+export default PokeDex
